de4c915f96640dfc50144e382beffc557210d477
Rafaël Blais Masson <rafbmasson@gmail.com>
2014-04-10 11:04:54 -0400

    Cleanup specs

--- spec/file_info_spec.rb
+++ spec/file_info_spec.rb
@@ -7,15 +7,21 @@ describe FileInfo do
   let(:macroman_file)   { fixture('encoding_macroman.csv') }
   let(:utf8_file)       { fixture('encoding_utf8.csv') }
 
-  describe '.load' do
-    it 'detects encoding from file' do
+  describe '#encoding' do
+    it 'returns Encoding instance' do
       expect(FileInfo.load(ascii_file.path).encoding).to      eq Encoding::US_ASCII
       expect(FileInfo.load(isolatin_file.path).encoding).to   eq Encoding::ISO_8859_1
       expect(FileInfo.load(isowindows_file.path).encoding).to eq Encoding::ISO_8859_1
       expect(FileInfo.load(utf8_file.path).encoding).to       eq Encoding::UTF_8
     end
 
-    it 'detects encoding from file with space in filename' do
+    it 'raises UnknownEncodingError' do
+      expect { FileInfo.load(macroman_file.path).encoding }.to raise_error(FileInfo::UnknownEncodingError)
+    end
+  end
+
+  describe '.load' do
+    it 'accepts filename with space' do
       old_filename = fixture('encoding_utf8.csv').path
       new_filename = old_filename.sub('_', ' ')
       FileUtils.cp(old_filename, new_filename)
@@ -25,7 +31,7 @@ describe FileInfo do
       FileUtils.rm(new_filename)
     end
 
-    it 'detects encoding from file with space and quote in filename' do
+    it 'accepts filename with space and quote' do
       old_filename = fixture('encoding_utf8.csv').path
       new_filename = old_filename.sub('_', ' " ')
       FileUtils.cp(old_filename, new_filename)
@@ -41,17 +47,11 @@ describe FileInfo do
   end
 
   describe '.parse' do
-    it 'detects encoding from string' do
+    it 'accepts a string' do
       expect(FileInfo.parse(ascii_file.read).encoding).to      eq Encoding::US_ASCII
       expect(FileInfo.parse(isolatin_file.read).encoding).to   eq Encoding::ISO_8859_1
       expect(FileInfo.parse(isowindows_file.read).encoding).to eq Encoding::ISO_8859_1
       expect(FileInfo.parse(utf8_file.read).encoding).to       eq Encoding::UTF_8
-    end
-  end
-
-  describe '#encoding' do
-    it 'raises UnknownEncodingError' do
-      expect { FileInfo.load(macroman_file.path).encoding }.to raise_error(FileInfo::UnknownEncodingError)
     end
   end
 end
